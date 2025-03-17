afb4b49349e5e502a2a254a91009a49600c222ab
Ahmed Fawzy <ahmed-fawzy@aucegypt.edu>
2025-03-17 06:58:33 +0200

    Profile CRUD with auth done

--- mern-auth/frontend/src/main.jsx
+++ mern-auth/frontend/src/main.jsx
@@ -14,18 +14,23 @@ import LoginScreen from './screens/LoginScreen.jsx';
 import RegisterScreen from './screens/RegisterScreen.jsx';
 import store from './store';
 import { Provider } from 'react-redux';
+import ProfileScreen from './screens/ProfileScreen.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
+
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route index={true} element={<HomeScreen />} />
+      <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
-
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 );
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
